640bceea840af17f51e85d8ee77c2abca70c7b44
wangsigui <wangsigui@yeah.net>
2018-03-26 15:57:48 +0800

    daytime client

--- chapter1/daytimecli.c
+++ chapter1/daytimecli.c
@@ -20,9 +20,14 @@ int main(int argc, char **argv)
     if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <= 0)
         err_quit("inet_pton error for %s",argv[1]);
     
+    //SA is struct sockaddr
+    if(connect(sockfd,(SA*)&servaddr,sizeof(servaddr)) < 0)
+        err_sys("connect error");
+
     while((n = read(sockfd,recvline,MAXLINE)) > 0)
     {
         recvline[n] = 0;       /*null terminate*/
+        printf("%d bytes.\n",n);
         if(fputs(recvline,stdout) == EOF)
             err_sys("fputs error");
     }
